8af8685878f6480e639e623f6a12bb96b4192705
Mahen-4 <99012598+Mahen-4@users.noreply.github.com>
2022-04-03 12:26:43 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from shared import shared
-
-PROJECTS = {
-    'ProjetTradingPythonPOO':{"path":('bot', 'Bot')},
-}
-
-shared.initialize_projects(PROJECTS)
-shared.play_candle_file("candle_sample.txt", PROJECTS)
-shared.display_gains(PROJECTS)
